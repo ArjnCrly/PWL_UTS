5887b55fd91f0bbd7b0bec29e57060fe0924f6f3
ArjnCrly <arjuna.carly1304@gmail.com>
2026-04-18 19:35:07 +0700

    feat: complete all resource form schemas

--- app/Filament/Resources/Levels/Schemas/LevelForm.php
+++ app/Filament/Resources/Levels/Schemas/LevelForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Levels\Schemas;
 
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class LevelForm
@@ -10,7 +11,15 @@ class LevelForm
     {
         return $schema
             ->components([
-                //
+                // Kode Level (Misal: ADM, STF)
+                TextInput::make('level_kode')
+                    ->required()
+                    ->maxLength(10),
+
+                // Nama Level (Misal: Administrator, Staff)
+                TextInput::make('level_nama')
+                    ->required()
+                    ->maxLength(100),
             ]);
     }
 }
--- app/Filament/Resources/Suppliers/Schemas/SupplierForm.php
+++ app/Filament/Resources/Suppliers/Schemas/SupplierForm.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\Suppliers\Schemas;
 
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 use Filament\Schemas\Schema;
 
 class SupplierForm
@@ -10,7 +12,20 @@ class SupplierForm
     {
         return $schema
             ->components([
-                //
+                // Kode Supplier
+                TextInput::make('supplier_kode')
+                    ->required()
+                    ->maxLength(10),
+
+                // Nama Supplier
+                TextInput::make('supplier_nama')
+                    ->required()
+                    ->maxLength(100),
+
+                // Alamat Supplier
+                Textarea::make('supplier_alamat')
+                    ->required()
+                    ->columnSpanFull(),
             ]);
     }
 }
