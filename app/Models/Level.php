9c666a49255289928d89c0e11295ff1aba0c7d5f
ArjnCrly <arjuna.carly1304@gmail.com>
2026-04-18 19:07:24 +0700

    fix: define custom table names in Models to resolve QueryException

--- app/Models/Level.php
+++ app/Models/Level.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Level extends Model
 {
-    //
+    protected $table = 'm_level'; 
+    protected $primaryKey = 'level_id'; 
+    protected $guarded = [];
 }
